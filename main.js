3b0f93b790b30be1d9dc16151b531648fc66d664
K-Ld <tp01045918974@gmail.com>
2022-05-18 16:05:41 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -6,12 +6,9 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-// const navbar = document.getElementById('navbar');
-// const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbar height: ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbar height: ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
@@ -19,3 +16,20 @@ document.addEventListener('scroll', () => {
     }
 })
 
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({
+        behavior: "smooth",
+    });
+});
