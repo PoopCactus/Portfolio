a71561cbdb7ad75266353edecf601e8f97dc45de
K-Ld <tp01045918974@gmail.com>
2022-05-25 13:42:41 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -38,11 +38,6 @@ homeContact.addEventListener('click', () => {
     scrollIntoView('#contact')
 });
 
-function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
 
 // Make home slowly fade to transparent as the window scrolls down
 
@@ -71,6 +66,40 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 })
+
+
+// Projects 
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        })
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+    
+    // for (let project of projects) {
+    // }
+    
+    // let project;
+    // for(let i = 0; i < projects.length; i++) {
+    //     project = projects[i];
+    // }
+});
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
