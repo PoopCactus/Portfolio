e751f52e9f6d3826b995228b970d45a2843a14dd
K-Ld <tp01045918974@gmail.com>
2022-05-26 13:03:06 +0900

    Final Touch!

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ document.addEventListener('scroll', () => {
 
 // Show "arrow up" button when scrolling down
 
-const arrowUp = document.querySelector('#arrow--up');
+const arrowUp = document.querySelector('#arrow-up');
 
 document.addEventListener('scroll', () => {
     if (window.scrollY > homeHeight / 2) {
