26cd8321fbe75fb3ba12e61f9c26b6b1793c219b
K-Ld <tp01045918974@gmail.com>
2022-05-27 15:42:19 +0900

    JS: Enable scroll menu.

--- main.js
+++ main.js
@@ -116,8 +116,77 @@ workBtnContainer.addEventListener('click', (e) => {
     // }
 });
 
+
+// 1. Brings in all section elements and menu items.
+// 2. All sections are observed using IntersectionObserver.
+// 3. Activates the menu item corresponding to the visible section.
+
+const sectionIds = [
+    '#home', 
+    '#about', 
+    '#skills', 
+    '#work', 
+    '#testimonial', 
+    '#contact',
+];
+// ▲ Storing all ids as strings in the sectionIds array as an array.
+const sections = sectionIds.map(id => document.querySelector(id));
+// ▲ Allocate all section elements to an array called sections.
+const navItems = sectionIds.map(id => 
+    document.querySelector(`[data-link="${id}"]`)
+);
+// ▲ Assign navigation menu item elements as navItems.
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+};
+// ▲ When a new menu item is selected, 
+// it deletes the previously activated element and assigns a new one to specify the .actve class.
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // Scrolling down to bring up the page.
+            if (entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    })
+};
+// ▲ Whenever a section goes out of the window using the Intersection Observer API, 
+// the index corresponding to the next is calculated and assigned.
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
+// ▲ When the user scrolls using the index, 
+// if the position is at the top, set the index at the top, 
+// if the position is in the middle, find the menu item using the navIndex calculated by IntersectionObserver,
+// and if it is the bottom, select the last menu item.
